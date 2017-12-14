17427c8717683407350564bb72bfb91b049a8d91
Klaus Wenger <kjwenger@yahoo.com>
2017-12-14 17:31:34 +0100

    moving to external projects

--- build.sh
+++ build.sh
@@ -4,13 +4,8 @@ sudo apt-get install cmake
 sudo apt-get install g++
 sudo apt-get install gcc
 
-cd src/lib/gtest-1.7.0
-./build.sh
-cd ../../..
-
 mkdir -p build
 cd build
 cmake ..
 cmake --build .
-./tests
 cd ..
