949b522911ff5b109a5cb2c267922540269065a9
Klaus Wenger <kjwenger@yahoo.com>
2018-02-01 17:04:45 +0100

    begin of restructuring

--- src/lib/cucumber-cpp-master/features/step_definitions/cucumber_cpp_steps.rb
+++
@@ -1,8 +0,0 @@
-Given /^a step definition file with:$/ do |code|
-    append_support_code code
-end
-
-Then /^the step output should contain:$/ do |output|
-    expect(@steps_out.gets).to eq(output)
-end
-
--- src/lib/gtest-1.7.0/build.cmd
+++
@@ -1,8 +0,0 @@
-@MKDIR build
-@CHDIR build
-@REM @CMAKE -Dgtest_build_samples=ON -G "MinGW Makefiles" ..
-@CMAKE -Dgtest_build_samples=ON ..
-@CMAKE --build .
-@COPY /B Debug\gtest.lib ..\..\..\..\lib\test
-@COPY /B Debug\gtest_main.lib ..\..\..\..\lib\test
-@CHDIR ..
