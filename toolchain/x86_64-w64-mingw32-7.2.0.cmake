b64e407cff2227a9ed40eba5078d70df93ecce33
Klaus Wenger <kjwenger@yahoo.com>
2018-02-06 23:15:07 +0100

    succeeding w/ MinGW 7.2.0 32-bit and 64-bit

--- toolchain/x86_64-w64-mingw32-7.2.0.cmake
+++ toolchain/x86_64-w64-mingw32-7.2.0.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_MAKE_PROGRAM "D:\\Development\\mingw-w64\\x86_64-7.2.0-posix-seh-rt_v5-rev1\\mingw64\\bin\\mingw64-make.exe")
+set(CMAKE_MAKE_PROGRAM "D:\\Development\\mingw-w64\\x86_64-7.2.0-posix-seh-rt_v5-rev1\\mingw64\\bin\\mingw32-make.exe")
 list(INSERT CMAKE_PROGRAM_PATH 0 "D:\\Development\\mingw-w64\\x86_64-7.2.0-posix-seh-rt_v5-rev1\\mingw64\\bin")
 set(CMAKE_C_COMPILER "gcc.exe")
 set(CMAKE_CXX_COMPILER "g++.exe")
